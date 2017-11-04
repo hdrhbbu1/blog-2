c82ea98b0a4d945242fb9a1cb59c624412be328f
Krzysztof Żuraw <krzysztof.zuraw@gmail.com>
2017-11-04 19:17:09 +0100

    Fix typos & remove analytics

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -10,7 +10,7 @@ class Comments extends React.Component {
           marginBottom: rhythm(1),
         }}
       >
-        I turned off Disqus comments. If you want to give me feeback please
+        I turned off Disqus comments. If you want to give me feedback please
         write to krzysztof.zuraw(at)fastmail.com or use{' '}
         <a href="https://keybase.io/encrypt#krzysztofzuraw">Keybase</a>.
       </p>
